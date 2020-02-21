0f12ed14a57033e5a5bfa25e58088a14d3dc44a0
Trimage <seokwon0127@naver.com>
2020-02-21 20:02:28 +0900

    메뉴 구성 리뉴얼

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "dbbase.h"
 #include "base.h"
+#define TAB 40
 
 using namespace std;
 
@@ -32,37 +33,36 @@ int main() {
 	cout << "★★SEOK_POS에 오신것을 환영합니다.★★\n\n\n";
 
 	while (1) {
-		cout << "\n\n┌────────────────────────────┐\n";
-		cout << setw(30) << left << "│ 메인메뉴" << "│\n";
-		cout << setw(30) << left << "│" << "│\n";
-		cout << setw(30) << left << "│ 1. 상품판매" << "│\n";
-		cout << setw(30) << left << "│ 2. 상품관리" << "│\n";
-		cout << setw(30) << left << "│ 3. 매출현황" << "│\n";
-		cout << setw(30) << left << "│ 4. 판매량추이" << "│\n";
-		cout << setw(30) << left << "│ 5. 환경설정" << "│\n";
-		cout << setw(30) << left << "│ 6. 프로그램 닫기" << "│\n";
-		cout << "└────────────────────────────┘\n";
+		cout << "\n\n┌──────────────────────────────────────┐\n";
+		cout << setw(TAB) << left << "│ 메인메뉴" << "│\n";
+		cout << setw(TAB) << left << "│" << "│\n";
+		cout << setw(TAB) << left << "│ 1. 영업하기" << "│\n";
+		cout << setw(TAB) << left << "│ 2. 상품관리" << "│\n";
+		cout << setw(TAB) << left << "│ 3. 매출관리" << "│\n";
+		cout << setw(TAB) << left << "│ 4. 환경설정" << "│\n";
+		cout << setw(TAB) << left << "│ 5. 프로그램 닫기" << "│\n";
+		cout << "└──────────────────────────────────────┘\n";
 		cout << "메뉴 선택 : ";
-		cin >> menu; // 1:상품판매 , 2:상품관리, 3:매출현황, 4:판매량추이, 5:환경설정, 6:프로그램 닫기
+		cin >> menu; // 1:영업하기 , 2:상품관리, 3:매출관리, 4:환경설정, 5:프로그램 닫기
 
 		if (cin.fail()) {
 			cin.clear(); //오류스트림을 초기화
 			cin.ignore(256, '\n'); //입력 버퍼를 지움
 		}
 
-		if (menu == 6) break;
+		if (menu == 5) break;
 
 		switch (menu) {
-		case 1:				// 메인메뉴 -> 상품판매 관련메뉴
+		case 1:				// 메인메뉴 -> 영업하기 관련메뉴
 			while (1) {
-				cout << "\n\n┌────────────────────────────┐\n";
-				cout << setw(30) << left << "│ 메인메뉴 -> 상품판매" << "│\n";
-				cout << setw(30) << left << "│" << "│\n";
-				cout << setw(30) << left << "│ 1. 상품판매 및 환불" << "│\n";
-				cout << setw(30) << left << "│ 2. 영수증조회" << "│\n";
-				cout << setw(30) << left << "│ 3. 일자별 영수증목록 보기" << "│\n";
-				cout << setw(30) << left << "│ 4. 이전메뉴로 돌아가기" << "│\n";
-				cout << "└────────────────────────────┘\n";
+				cout << "\n\n┌──────────────────────────────────────┐\n";
+				cout << setw(TAB) << left << "│ 메인메뉴 -> 영업하기" << "│\n";
+				cout << setw(TAB) << left << "│" << "│\n";
+				cout << setw(TAB) << left << "│ 1. 상품판매 및 환불" << "│\n";
+				cout << setw(TAB) << left << "│ 2. 영수증조회" << "│\n";
+				cout << setw(TAB) << left << "│ 3. 일자별 영수증목록 보기" << "│\n";
+				cout << setw(TAB) << left << "│ 4. 이전메뉴로 돌아가기" << "│\n";
+				cout << "└──────────────────────────────────────┘\n";
 				cout << "메뉴 선택 : ";
 				cin >> menu; // 1:상품판매, 2.영수증조회, 3. 일자별 영수증목록 보기, 4.이전메뉴로 돌아가기
 
@@ -92,16 +92,16 @@ int main() {
 
 		case 2:				// 메인메뉴 -> 상품관리 관련메뉴
 			while (1) {
-				cout << "\n\n┌────────────────────────────┐\n";
-				cout << setw(30) << left << "│ 메인메뉴 -> 상품관리" << "│\n";
-				cout << setw(30) << left << "│" << "│\n";
-				cout << setw(30) << left << "│ 1. 상품등록" << "│\n";
-				cout << setw(30) << left << "│ 2. 상품수정" << "│\n";
-				cout << setw(30) << left << "│ 3. 상품제거" << "│\n";
-				cout << setw(30) << left << "│ 4. 특정상품 정보조회" << "│\n";
-				cout << setw(30) << left << "│ 5. 모든상품 정보조회" << "│\n";
-				cout << setw(30) << left << "│ 6. 이전메뉴로 돌아가기" << "│\n";
-				cout << "└────────────────────────────┘\n";
+				cout << "\n\n┌──────────────────────────────────────┐\n";
+				cout << setw(TAB) << left << "│ 메인메뉴 -> 상품관리" << "│\n";
+				cout << setw(TAB) << left << "│" << "│\n";
+				cout << setw(TAB) << left << "│ 1. 상품등록" << "│\n";
+				cout << setw(TAB) << left << "│ 2. 상품수정" << "│\n";
+				cout << setw(TAB) << left << "│ 3. 상품제거" << "│\n";
+				cout << setw(TAB) << left << "│ 4. 특정상품 정보조회" << "│\n";
+				cout << setw(TAB) << left << "│ 5. 모든상품 정보조회" << "│\n";
+				cout << setw(TAB) << left << "│ 6. 이전메뉴로 돌아가기" << "│\n";
+				cout << "└──────────────────────────────────────┘\n";
 				cout << "메뉴 선택 : ";
 				cin >> menu; // 1:상품등록, 2.상품수정, 3.상품제거, 4.특정상품조회, 5.모든상품조회, 6.이전메뉴로 돌아가기
 
@@ -135,40 +135,108 @@ int main() {
 			}
 			break;
 
-		case 3:				// 메인메뉴 -> 매출현황 관련메뉴
+		case 3:				// 메인메뉴 -> 매출관리 관련메뉴
 			while (1) {
-				cout << "\n\n┌────────────────────────────┐\n";
-				cout << setw(30) << left << "│ 메인메뉴 -> 매출현황" << "│\n";
-				cout << setw(30) << left << "│" << "│\n";
-				cout << setw(30) << left << "│ 1. 금일 매출현황" << "│\n";
-				cout << setw(30) << left << "│ 2. 특정일 매출현황" << "│\n";
-				cout << setw(30) << left << "│ 3. 월별 매출현황" << "│\n";
-				cout << setw(30) << left << "│ 4. 년별 매출현황" << "│\n";
-				cout << setw(30) << left << "│ 5. 이전메뉴로 돌아가기" << "│\n";
-				cout << "└────────────────────────────┘\n";
+				cout << "\n\n┌──────────────────────────────────────┐\n";
+				cout << setw(TAB) << left << "│ 메인메뉴 -> 매출관리" << "│\n";
+				cout << setw(TAB) << left << "│" << "│\n";
+				cout << setw(TAB) << left << "│ 1. 매출 통계" << "│\n";
+				cout << setw(TAB) << left << "│ 2. 판매량 통계" << "│\n";
+				cout << setw(TAB) << left << "│ 3. 이전메뉴로 돌아가기" << "│\n";
+				cout << "└──────────────────────────────────────┘\n";
 				cout << "메뉴 선택 : ";
-				cin >> menu; // 1:금일매출현황, 2.특정일 매출현황, 3.월별매출현황, 4.연별매출현황, 5.이전메뉴로 돌아가기
+				cin >> menu; // 1:매출 통계, 2.판매량 통계, 3.이전메뉴로 돌아가기
 
 				if (cin.fail()) {
 					cin.clear(); //오류스트림을 초기화
 					cin.ignore(256, '\n'); //입력 버퍼를 지움
 				}
 
-				if (menu == 5) break;
+				if (menu == 3) break;
 
 				switch (menu) {
-				case 1:
-					profit.current_view(podbc);
+				case 1:			// 메인메뉴 -> 매출관리 -> 매출통계 관련메뉴
+					while (1) {
+						cout << "\n\n┌──────────────────────────────────────┐\n";
+						cout << setw(TAB) << left << "│ 메인메뉴 -> 매출관리 -> 매출통계" << "│\n";
+						cout << setw(TAB) << left << "│" << "│\n";
+						cout << setw(TAB) << left << "│ 1. 금일 매출현황" << "│\n";
+						cout << setw(TAB) << left << "│ 2. 특정일 매출현황" << "│\n";
+						cout << setw(TAB) << left << "│ 3. 월별 매출현황" << "│\n";
+						cout << setw(TAB) << left << "│ 4. 년별 매출현황" << "│\n";
+						cout << setw(TAB) << left << "│ 5. 이전메뉴로 돌아가기" << "│\n";
+						cout << "└──────────────────────────────────────┘\n";
+						cout << "메뉴 선택 : ";
+						cin >> menu; // 1:금일매출현황, 2.특정일 매출현황, 3.월별매출현황, 4.연별매출현황, 5.이전메뉴로 돌아가기
+
+						if (cin.fail()) {
+							cin.clear(); //오류스트림을 초기화
+							cin.ignore(256, '\n'); //입력 버퍼를 지움
+						}
+
+						if (menu == 5) break;
+
+						switch (menu) {
+						case 1:
+							profit.current_view(podbc);
+							break;
+						case 2:
+							profit.day_view(podbc);
+							break;
+						case 3:
+							profit.month_view(podbc);
+							break;
+						case 4:
+							profit.year_view(podbc);
+							break;
+						default:
+							cout << "\n올바른 값을 입력해주세요.\n\n";
+							break;
+						}
+					}
 					break;
-				case 2:
-					profit.day_view(podbc);
+
+				case 2:			// 메인메뉴 -> 매출관리 -> 판매량 통계 관련메뉴
+					while (1) {
+						cout << "\n\n┌──────────────────────────────────────┐\n";
+						cout << setw(TAB) << left << "│ 메인메뉴 -> 매출관리 -> 판매량 통계" << "│\n";
+						cout << setw(TAB) << left << "│" << "│\n";
+						cout << setw(TAB) << left << "│ 1. 금일 판매량 추이" << "│\n";
+						cout << setw(TAB) << left << "│ 2. 특정일 판매량 추이" << "│\n";
+						cout << setw(TAB) << left << "│ 3. 월별 판매량 추이" << "│\n";
+						cout << setw(TAB) << left << "│ 4. 년별 판매량 추이" << "│\n";
+						cout << setw(TAB) << left << "│ 5. 이전메뉴로 돌아가기" << "│\n";
+						cout << "└──────────────────────────────────────┘\n";
+						cout << "메뉴 선택 : ";
+						cin >> menu; // 1:금일 판매량 추이, 2:특정일 판매량 추이, 3:월별 판매량 추이, 4:년별 판매량 추이, 5.이전메뉴로 돌아가기
+
+						if (cin.fail()) {
+							cin.clear(); //오류스트림을 초기화
+							cin.ignore(256, '\n'); //입력 버퍼를 지움
+						}
+
+						if (menu == 5) break;
+
+						switch (menu) {
+						case 1:
+							receipt.current_state(podbc);
+							break;
+						case 2:
+							receipt.day_state(podbc);
+							break;
+						case 3:
+							receipt.month_state(podbc);
+							break;
+						case 4:
+							receipt.year_state(podbc);
+							break;
+						default:
+							cout << "\n올바른 값을 입력해주세요.\n\n";
+							break;
+						}
+					}
 					break;
-				case 3:
-					profit.month_view(podbc);
-					break;
-				case 4:
-					profit.year_view(podbc);
-					break;
+
 				default:
 					cout << "\n올바른 값을 입력해주세요.\n\n";
 					break;
@@ -176,57 +244,16 @@ int main() {
 			}
 			break;
 
-		case 4:				// 메인메뉴 -> 통계
+		case 4:			// 메인메뉴 -> 환경설정
 			while (1) {
-				cout << "\n\n┌────────────────────────────┐\n";
-				cout << setw(30) << left << "│ 메인메뉴 -> 판매량 추이" << "│\n";
-				cout << setw(30) << left << "│" << "│\n";
-				cout << setw(30) << left << "│ 1. 금일 판매량 추이" << "│\n";
-				cout << setw(30) << left << "│ 2. 특정일 판매량 추이" << "│\n";
-				cout << setw(30) << left << "│ 3. 월별 판매량 추이" << "│\n";
-				cout << setw(30) << left << "│ 4. 년별 판매량 추이" << "│\n";
-				cout << setw(30) << left << "│ 5. 이전메뉴로 돌아가기" << "│\n";
-				cout << "└────────────────────────────┘\n";
-				cout << "메뉴 선택 : ";
-				cin >> menu; // 1:금일 판매량 추이, 2:특정일 판매량 추이, 3:월별 판매량 추이, 4:년별 판매량 추이, 5.이전메뉴로 돌아가기
-
-				if (cin.fail()) {
-					cin.clear(); //오류스트림을 초기화
-					cin.ignore(256, '\n'); //입력 버퍼를 지움
-				}
-
-				if (menu == 5) break;
-
-				switch (menu) {
-				case 1:
-					receipt.current_state(podbc);
-					break;
-				case 2:
-					receipt.day_state(podbc);
-					break;
-				case 3:
-					receipt.month_state(podbc);
-					break;
-				case 4:
-					receipt.year_state(podbc);
-					break;
-				default:
-					cout << "\n올바른 값을 입력해주세요.\n\n";
-					break;
-				}
-			}
-			break;
-
-		case 5:			// 메인메뉴 -> 환경설정
-			while (1) {
-				cout << "\n\n┌────────────────────────────┐\n";
-				cout << setw(30) << left << "│ 메인메뉴 -> 환경설정" << "│\n";
-				cout << setw(30) << left << "│" << "│\n";
-				cout << setw(30) << left << "│ 1. 포스번호 변경" << "│\n";
-				cout << setw(30) << left << "│ 2. 로그인 정보 확인" << "│\n";
-				cout << setw(30) << left << "│ 3. 다른 계정으로 로그인하기" << "│\n";
-				cout << setw(30) << left << "│ 4. 이전메뉴로 돌아가기" << "│\n";
-				cout << "└────────────────────────────┘\n";
+				cout << "\n\n┌──────────────────────────────────────┐\n";
+				cout << setw(TAB) << left << "│ 메인메뉴 -> 환경설정" << "│\n";
+				cout << setw(TAB) << left << "│" << "│\n";
+				cout << setw(TAB) << left << "│ 1. 포스번호 변경" << "│\n";
+				cout << setw(TAB) << left << "│ 2. 로그인 정보 확인" << "│\n";
+				cout << setw(TAB) << left << "│ 3. 다른 계정으로 로그인하기" << "│\n";
+				cout << setw(TAB) << left << "│ 4. 이전메뉴로 돌아가기" << "│\n";
+				cout << "└──────────────────────────────────────┘\n";
 				cout << "메뉴 선택 : ";
 				cin >> menu; // 1:포스번호 변경, 2.로그인 정보 확인, 3.다른 계정으로 로그인하기, 4.이전메뉴로 돌아가기
 
